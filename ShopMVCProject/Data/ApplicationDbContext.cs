c29a11f86471ffcc11cd9d0000c57bc7910f5cdc
Solka99 <alicjas508@gmail.com>
2024-06-02 17:07:40 +0200

    Added DbContext

---
+++ ShopMVCProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShopMVCProject.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+                
+        }
+    }
+}
