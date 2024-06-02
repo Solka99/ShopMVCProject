7e3e00a55dcf83f8abd77cc9a032f64a560535a7
Solka99 <alicjas508@gmail.com>
2024-06-02 15:55:29 +0200

    Add project files.

---
+++ ShopMVCProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ShopMVCProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
