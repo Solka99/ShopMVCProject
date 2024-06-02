aa2c9e81dcb758e1752fdb06ef389d8adb5a51c1
Solka99 <alicjas508@gmail.com>
2024-06-02 21:26:02 +0200

    New Funcitonalities

--- ShopMVCProject/Controllers/CategoryController.cs
+++ ShopMVCProject/Controllers/CategoryController.cs
@@ -1,12 +1,92 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShopMVCProject.Data;
+using ShopMVCProject.Models;
 
 namespace ShopMVCProject.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _dbcontext;
+        public CategoryController(ApplicationDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
         public IActionResult Index()
+        {
+            List<Category> objCategoryList=_dbcontext.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
+        [HttpPost]
+        public IActionResult Create(Category categoryObj)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbcontext.Categories.Add(categoryObj);
+                _dbcontext.SaveChanges();
+                TempData["success"] = "Category created successfully";
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+            Category categoryFromDb= _dbcontext.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category categoryObj)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbcontext.Categories.Update(categoryObj);
+                _dbcontext.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category categoryFromDb = _dbcontext.Categories.Find(id);
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Category obj=_dbcontext.Categories.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            _dbcontext.Categories.Remove(obj);
+            _dbcontext.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+            return RedirectToAction("Index", "Category");
+            
+        }
+
+
     }
 }
